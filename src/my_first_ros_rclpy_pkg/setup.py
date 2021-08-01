7a3b55920e644aac9f293231a6392e0ef76be899
ghtjd1492 <ghtjd1492@gmail.com>
2021-08-02 01:36:08 +0900

    first commit : create ros python pkg

---
+++ src/my_first_ros_rclpy_pkg/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'my_first_ros_rclpy_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ghtjd1492',
+    maintainer_email='ghtjd1492@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='ROS 2 rclpy basic package for the ROS 2 seminar',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'helloworld_publisher = my_first_ros_rclpy_pkg.helloworld_publisher:main',
+            'helloworld_subscriber = my_first_ros_rclpy_pkg.helloworld_subscriber:main',
+        ],
+    },
+)
